0fbe8a653e738deeafe43300b5c7ad3995204000
Bill O'Connell <91283923+wdoconnell@users.noreply.github.com>
2022-01-05 10:41:45 -0500

    Update db-models.js

--- server/db-models/db-models.js
+++ server/db-models/db-models.js
@@ -1,11 +1,6 @@
 const { Pool } = require('pg');
 
-<<<<<<< HEAD
 require('dotenv').config();
-=======
-import * as dotenv from 'dotenv';
-dotenv.config();
->>>>>>> dev
 
 const PG_URI = process.env.PG_URI;
 
@@ -13,16 +8,8 @@ const pool = new Pool({
   connectionString: PG_URI,
 });
 
-<<<<<<< HEAD
 module.exports = {
   query: (text, params, callback) => {
     return pool.query(text, params, callback);
   },
 };
-=======
-const query = (text, params, callback) => {
-  return pool.query(text, params, callback);
-};
-
-export default query;
->>>>>>> dev
